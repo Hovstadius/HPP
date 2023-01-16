9bf2a7f03c997a01da555b316b1656f86fba378b
Hovstadius <115700333+Hovstadius@users.noreply.github.com>
2023-01-16 18:03:13 +0100

    Add files via upload

---
+++ ctest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+
+    float a = 0.1;
+    float b = 0.2;
+    float c = a + b;
+    printf("%f\n",a);
+    printf("%f",b);
+    printf("%f",c);
+    return 0;
+}
